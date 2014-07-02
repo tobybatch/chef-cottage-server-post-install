9f6d46526f66ba29ffc31dbd2f84dcf4ea6b4eb5
Toby Batch <tobias@neontribe.co.uk>
2014-07-02 23:03:44 +0100

    Initial import

---
+++ attributes/default.rb
@@ -0,0 +1,6 @@
+default['pound']['user'] = "haproxy"
+default['pound']['group'] = "haproxy"
+default['pound']['loglevel'] = "haproxy"
+default['pound']['alive'] = "haproxy"
+default['pound']['control'] = "haproxy"
+
---
+++ metadata.rb
@@ -0,0 +1,13 @@
+name             'pound'
+maintainer       'Toby Batch.'
+maintainer_email 'tobias@neontribe.co.uk'
+license          'GPL 2'
+description      'Installs xml'
+version          '0.0.1'
+
+depends 'apt'
+
+supports 'ubuntu'
+
+recipe 'pound', 'Install the pound load balancer'
+
