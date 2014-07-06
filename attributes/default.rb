b7223cee28080d5442cff231f7f1ffa39d0379ca
Toby Batch <tobias@neontribe.co.uk>
2014-07-06 14:47:00 +0100

    Added attributes and default config

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,11 @@
-default['pound']['user'] = "haproxy"
-default['pound']['group'] = "haproxy"
-default['pound']['loglevel'] = "haproxy"
-default['pound']['alive'] = "haproxy"
-default['pound']['control'] = "haproxy"
+default['pound']['user'] = "www-data"
+default['pound']['group'] = "www-data"
+default['pound']['loglevel'] = "1"
+default['pound']['alive'] = "30"
+default['pound']['control'] = "/var/run/pound/poundctl.socket"
+default['pound']['listenaddr'] = "127.0.0.1"
+default['pound']['listenport'] = "8080"
+default['pound']['backendaddr'] = ["127.0.0.1", 192.168.51.31]
+default['pound']['backendport'] = ["80", "443"]
+default['pound']['cert'] = "/etc/ssl/certs/ssl-cert.pem"
 
